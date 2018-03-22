ca8c8ab750d7bffc844dee1d4bb5adf075666f6c
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-22 09:38:28 -0500

    Finish v1 timer page

--- src/pages/timer/timer.js
+++ src/pages/timer/timer.js
@@ -1,16 +1,18 @@
 import * as $ from 'jquery';
 import {
-  ipcRenderer
+  ipcRenderer,
+  remote
 } from 'electron';
 
 let timeLeft = 0;
 let duration;
-let cont = true;
+let cont = false; // "Continue" variable initializes to false, then is turned true by startTimer()
 
-updateTimeView(timeLeft)
+updateTimeView(timeLeft) // Initially establish time on the page
 
+// Recieve the preset data from the main project; it was sent by the index page
 ipcRenderer.on('preset', (event, args) => {
-  duration = args || 0
+  duration = args || 0 // If no preset was sent, default time is zero
   timeLeft = duration
   updateTimeView(timeLeft)
 })
@@ -18,13 +20,35 @@ ipcRenderer.on('preset', (event, args) => {
 $('.startButton').click(function () {
   startTimer(duration)
 })
+
 $('.stopButton').click(() => {
-  cont = false
+  stopTimer()
 })
 
-// TODO: add support for up and down arrows and changing the duration based on that.
-// Perhaps consider changing code, then updating html instead of basing code on retrieveing html value
+// Increases the time on the timer
+$(".up").click(function () {
+  stopTimer() // Stop the timer before making any changes
+  duration = Math.round(duration / 1000) * 1000
+  duration += 1000
+  timeLeft = duration
+  updateTimeView(timeLeft)
+})
 
+
+$(".down").click(function () {
+  stopTimer() // Stop the timer before making any changes
+  duration = Math.round(duration / 1000) * 1000
+  duration -= 1000
+  timeLeft = duration
+  updateTimeView(timeLeft)
+})
+
+// Close the window; this is because there are no control buttons
+$(".exit").click(function () {
+  remote.getCurrentWindow().close()
+})
+
+// Turns epoc time into a time string (mm:ss.sss)
 function parseTime(epocNum) {
   let d = new Date(epocNum)
   let iso = d.toISOString()
@@ -32,12 +56,15 @@ function parseTime(epocNum) {
   return parsed
 }
 
+// Updates the time on the html page
 function updateTimeView(epocTime) {
   let timeUpdate = parseTime(epocTime)
   $('.time').text(timeUpdate)
 }
 
+// Start the timer
 function startTimer(duration) {
+  cont = true
   let startTime = new Date().now()
   while (cont) {
     setTimeout(() => {
@@ -45,4 +72,9 @@ function startTimer(duration) {
     }, 25)
     updateTimeView(timeLeft)
   }
+}
+
+// Stops the timer
+function stopTimer() {
+  cont = false
 }
